a251a9db119c62cb9862654dd3f46dbef3eeb1cb
Septi <septimiu.ujica@gmail.com>
2025-06-21 15:24:08 +0300

    updated docker package install method

--- sentinel_hl/info.py
+++ sentinel_hl/info.py
@@ -1,6 +1,11 @@
+import os
+
 __app_name__ = "Sentinel-Hl"
 __package_name__ = "sentinel-hl"
-__version__ = "1.0.12"
+
+with open(os.path.join(os.path.dirname(__file__), "VERSION"), "r") as version_file:
+    __version__ = version_file.read().strip()
+
 __description__ = "A simple tool that watches over your infrastructure and ensures that all systems are running."
 __author__ = "Septimiu Ujica"
 __author_email__ = "hellp@septi.ro"
